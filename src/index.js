69d2b145aa1d9a532be1967295f9914f6cbafc19
Anna Levkovich <7604442@gmail.com>
2022-11-25 13:50:22 +0300

    init gh-pages

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { Game } from "./game";
-//запускаем игру
-window.onload = () => {
-    const electronika = new Game();
-    electronika.run();
-}
-console.log('Работает');
- 
-console.log('не работает');
